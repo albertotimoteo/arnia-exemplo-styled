f46631d44f88a08d12c1c6e58f54be4fccb6b00b
Alberto Timoteo <alberto.pptimoteo@gmail.com>
2023-08-08 19:44:09 -0300

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,41 @@
-import Accordion from "./components/Accordion"
-import Card from "./components/Card"
+const pessoas = [
+  {
+    id: "293b52d6-a4f8-4c60-837e-a819e4a221ac",
+    nome: "Alberto",
+    idade: 29,
+    profissao: "Programador",
+  }, // 0
+  {
+    id: "f9b3ee03-aa8e-4bff-8790-3f93cf081257",
+    nome: "José",
+    idade: 40,
+    profissao: "Motorista",
+  }, // 1
+  {
+    id: "086245d8-282f-40a7-8fba-fa79cd8e40fa",
+    nome: "Henrique",
+    idade: 18,
+    profissao: "Estudante",
+  }, // 2
+  {
+    id: "6d5f5343-5c3c-494b-a504-cef23e97bad3",
+    nome: "Ana",
+    idade: 35,
+    profissao: "CEO",
+  }, // 3
+]
 
 function App() {
   return (
     <>
-      <Card>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo consectetur
-        tenetur deserunt rerum totam iure aperiam veniam quis dolorum eos?
-      </Card>
-      <br />
-      <Accordion title="Titulo">
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo
-          consectetur tenetur deserunt rerum totam iure aperiam veniam quis
-          dolorum eos?
-        </p>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt,
-          ducimus!
-        </p>
-      </Accordion>
+      {pessoas.map((pessoa, index) => (
+        <div key={index}>
+          <p>
+            {pessoa.nome} - {pessoa.idade} anos
+          </p>
+          <p>{pessoa.profissao}</p>
+        </div>
+      ))}
     </>
   )
 }
